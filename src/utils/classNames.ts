959a6517c7f256e23053d9ca751ddfcb09937afb
Luke Kozak <kozakluke@gmail.com>
2018-04-06 17:53:24 +0200

    Fix classNames, dont add empty char on end

--- src/utils/classNames.ts
+++ src/utils/classNames.ts
@@ -3,10 +3,13 @@
  */
 export default function classNames(...args:string[])
 {
-    var names = '';
-    for (var i = 0, n = args.length; i < n; i++)
-        if (args[i])
+    let names = '';
+    for (let i = 0, n = args.length; i < n; i++) {
+        if (args[i] && i != n - 1)
             names += args[i] + ' ';
+        else if (args[i])
+            names += args[i];
+    }
     
     return names;
 }
