d59da823866b2a7cb9b70e2f020688d022e72fb0
Luke Kozak <kozakluke@gmail.com>
2018-04-08 13:14:04 +0200

    Update className.test, remove describe

--- tests/utils/classNames.test.ts
+++ tests/utils/classNames.test.ts
@@ -1,17 +1,10 @@
 import classNames from '../../src/utils/classNames';
 
-describe('classNames', ()=> {
-    test('test1', ()=>
-        expect(classNames('class1', 'class2')).toBe('class1 class2'));
-    test('test2', ()=> {
-        const data = {
-          val1: true,
-          val2: false  
-        };
-        expect(classNames(
-            data.val1 && 'class1',
-            data.val2 && 'class2',
-            'class3'
-        )).toBe('class1 class3')
-    });
+test('classNames', ()=> {
+    expect(classNames('class1', 'class2')).toBe('class1 class2');
+    expect(classNames(
+        !!1 && 'class1',
+        !!0 && 'class2',
+        'class3'
+    )).toBe('class1 class3')
 });
