15a3a0643b054d0d11a992138a6bf2a7668a3ffa
Luke Kozak <kozakluke@gmail.com>
2018-08-06 08:09:53 +0200

    Style remove utils dir from src

---
+++ index.ts
@@ -0,0 +1,14 @@
+import ArrayUtil from './src/ArrayUtil';
+import BezierUtil from './src/BezierUtil';
+import Cursor from './src/Cursor';
+import MathUtil from './src/MathUtil';
+import Pool from './src/Pool';
+import range from './src/range';
+import StringUtil from './src/StringUtil';
+import classNames from './src/classNames';
+import DateUtil from './src/DateUtil';
+/**
+ * @author kozakluke@gmail.com
+ */
+console.log(DateUtil.getMonthName(new Date()));
+console.log(DateUtil.getMonthShortName(new Date()));
