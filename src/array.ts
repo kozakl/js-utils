4bf98e3c0729fcc9d13335d4ed7149bd7513890a
Luke Kozak <kozakluke@gmail.com>
2018-10-08 18:13:14 +0200

    Create array util with export functions

---
+++ src/array.ts
@@ -0,0 +1,21 @@
+export function shuffle<T>(array:T[])
+{
+    for (let i = array.length; i--;) {
+        const j = Math.random() * (i + 1) | 0,
+              t = array[i];
+        array[i] = array[j];
+        array[j] = t;
+    }
+    
+    return array;
+}
+
+export function removeNull<T>(array:T[])
+{
+    for (let i = array.length; i--;) {
+        if (!array[i])  
+            array.splice(i, 1);
+    }
+    
+    return array;
+}
