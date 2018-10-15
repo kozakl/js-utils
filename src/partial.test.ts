003731496da4abe74dc1ba147d9f2b24abe85dac
Luke Kozak <kozakluke@gmail.com>
2018-10-15 19:40:11 +0200

    Fix partial test decoration

--- src/partial.test.ts
+++ src/partial.test.ts
@@ -3,7 +3,7 @@ import partial from './partial';
 const inc = (num:number)=> num + 1,
       add = (a:number, b:number)=> a + b;
 
-describe('partial', ()=> {
+test('partial', ()=> {
     expect(partial(inc, 10)()).toBe(11);
     expect(partial(add, 10, 10)()).toBe(20);
 });
