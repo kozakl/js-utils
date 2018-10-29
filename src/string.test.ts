1a6bd8a46c7da16a5a6b04048ed8aefdc5895972
Luke Kozak <kozakluke@gmail.com>
2018-10-29 23:08:17 +0100

    Add test for upperFirst from string util

--- src/string.test.ts
+++ src/string.test.ts
@@ -1,7 +1,13 @@
-import {lowerFirst} from './string';
+import {lowerFirst, upperFirst} from './string';
 
 test('lowerFirst', ()=> {
     expect(lowerFirst('sample')).toEqual('sample');
     expect(lowerFirst('Sample')).toEqual('sample');
     expect(lowerFirst('SAMPLE')).toEqual('sAMPLE');
+});
+
+test('upperFirst', ()=> {
+    expect(upperFirst('Sample')).toEqual('Sample');
+    expect(upperFirst('sample')).toEqual('Sample');
+    expect(upperFirst('SAMPLE')).toEqual('SAMPLE');
 });
