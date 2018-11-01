c0ea8606093fe0cd1ad7c3897ab03268591b4837
Luke Kozak <kozakluke@gmail.com>
2018-11-01 18:33:19 +0100

    Add filter as optional argument in listFiles

--- src/file.ts
+++ src/file.ts
@@ -1,11 +1,13 @@
 import {readdirSync, statSync} from 'fs-extra';
 import {extname, join} from 'path';
 
-export function listFiles(dir:string, filter:string[], files:string[] = [])
+export function listFiles(dir:string, filter?:string[], files:string[] = [])
 {
     readdirSync(dir).forEach((file)=> {
         if (statSync(join(dir, file)).isDirectory())
             listFiles(join(dir, file), filter, files);
+        else if (!filter)
+            files.push(join(dir, file));
         else if (filter.includes(extname(file)))
             files.push(join(dir, file));
     });
