a9bd721b2cc8f1314a737920de7c3ec3d0c821a4
Luke Kozak <kozakluke@gmail.com>
2018-11-24 13:01:39 +0100

    Add test for pad2 from string util

--- src/string.test.ts
+++ src/string.test.ts
@@ -1,5 +1,5 @@
 import {lowerFirst, upperFirst,
-        removeSubstr, pad1} from './string';
+        removeSubstr, pad1, pad2} from './string';
 
 test('lowerFirst', ()=> {
     expect(lowerFirst('sample')).toEqual('sample');
@@ -23,4 +23,10 @@ test('pad1', ()=> {
     expect(pad1(1)).toEqual('01');
     expect(pad1(10)).toEqual('10');
     expect(pad1(100)).toEqual('100');
+});
+
+test('pad2', ()=> {
+    expect(pad2(1)).toEqual('001');
+    expect(pad2(10)).toEqual('010');
+    expect(pad2(100)).toEqual('100');
 });
