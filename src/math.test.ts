34a1f7fc5d1e1d173c85bc0744780a2f2932e56d
Luke Kozak <kozakluke@gmail.com>
2018-11-25 21:01:42 +0100

    Add test for rndIntRange from math

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,7 +1,13 @@
-import {rndRange} from './math';
+import {rndRange, rndIntRange} from './math';
 
 test('rndRange', ()=> {
     expect(rndRange(0, 0.5)).toBeGreaterThanOrEqual(0);
     expect(rndRange(0, 0.5)).toBeLessThanOrEqual(0.5);
     expect(rndRange(0.15, 0.155)).toBeCloseTo(0.15);
+});
+
+test('rndIntRange', ()=> {
+    expect(rndIntRange(0, 5)).toBeGreaterThanOrEqual(0);
+    expect(rndIntRange(0, 5)).toBeLessThanOrEqual(5);
+    expect(rndIntRange(5, 5)).toBe(5);
 });
