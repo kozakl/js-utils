6275af9dd6e277ffa0e78333305999aa03e915fb
Luke Kozak <kozakluke@gmail.com>
2018-11-28 22:18:40 +0100

    Clean pipe.test

--- src/pipe.test.ts
+++ src/pipe.test.ts
@@ -5,14 +5,8 @@ const add = (a:number, b:number)=> a + b,
       mul = (a:number, b:number)=> a * b,
       inc = (num:number)=> num + 1;
 
-describe('pipe', ()=> {
-    test('test1 single method', ()=> {
-        expect(pipe(add)(5, 5)).toBe(10);
-    });
-    test('test2 multiple methods', ()=> {
-        expect(pipe(add, inc)(5, 5)).toBe(11);
-    });
-    test('test3 with partial', ()=> {
-        expect(pipe(add, partial(mul, 5))(5, 5)).toBe(50);
-    });
+test('pipe', ()=> {
+    expect(pipe(add, partial(mul, 5))(5, 5)).toBe(50);
+    expect(pipe(add)(5, 5)).toBe(10);
+    expect(pipe(add, inc)(5, 5)).toBe(11);
 });
