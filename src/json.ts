eba75bab4a77a66421379223fd65a4367cbddc6d
Luke Kozak <kozakluke@gmail.com>
2018-12-01 08:21:04 +0100

    Set transformDatesNormalized as deprecated

--- src/json.ts
+++ src/json.ts
@@ -11,11 +11,8 @@ export function transformDates<T extends ContainDate>(data:T[] & Normalized<T>, 
     return data;
 }
 
-export function transformDatesNormalized<T extends ContainDate>(data:Normalized<T>)
-{
-    data.all.map((id)=>
-        data.byId[id].date = new Date(data.byId[id].date.toString()));
-    return data;
+export function transformDatesNormalized<T extends ContainDate>(data:Normalized<T>) {
+    console.warn('json/transformDatesNormalized is deprecated use json/transformDates instead');
 }
 
 interface ContainDate {
