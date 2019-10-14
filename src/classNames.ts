dddee74286b5a26ad8d4c301a9401271e6324cc3
Luke Kozak <kozakluke@gmail.com>
2019-10-14 13:45:05 +0200

    Add brackets to classNames

--- src/classNames.ts
+++ src/classNames.ts
@@ -2,10 +2,11 @@ export default function classNames(...args:string[])
 {
     let names = '';
     for (let i = 0, n = args.length; i < n; i++) {
-        if (args[i] && i != n - 1)
+        if (args[i] && i != n - 1) {
             names += args[i] + ' ';
-        else if (args[i])
+        } else if (args[i]) {
             names += args[i];
+        }
     }
     
     return names;
