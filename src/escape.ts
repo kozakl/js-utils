4325334309d31c939ea227d5d7e53f02c150e467
Luke Kozak <kozakluke@gmail.com>
2019-10-14 13:46:05 +0200

    Set escape as deprecated

--- src/escape.ts
+++ src/escape.ts
@@ -1,5 +1,6 @@
 export default function escape(obj:any)
 {
+    console.log('escape is deprecated now, use quote instead');
     if (typeof obj === 'object') {
         for (const key in obj) {
             if (obj.hasOwnProperty(key)) {
