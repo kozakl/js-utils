48cda27b3e0aaccb0af5c643a340fa4fc3c499f9
Luke Kozak <kozakluke@gmail.com>
2019-10-15 06:28:42 +0200

    Add brackets

--- src/range.ts
+++ src/range.ts
@@ -1,8 +1,9 @@
 export default function range(start:number, stop:number, step = 1)
 {
     const array:number[] = [];
-    for (let i = start; i <= stop; i += step)
+    for (let i = start; i <= stop; i += step) {
         array.push(i);
+    }
     
     return array;
 }
