f4f90a67a2484149d031a6e19fe9428e926919d8
Luke Kozak <kozakluke@gmail.com>
2019-10-15 06:03:44 +0200

    Remove toBoolean from validate util

--- src/validate.ts
+++ src/validate.ts
@@ -12,18 +12,6 @@ export function isEmail(str:string)
     return /\S+@\S+\.\S+/.test(str);
 }
 
-export function toBoolean(str:string)
-{
-    console.log('toBoolean is deprecated now, use validateBoolean instead');
-    switch (str) {
-        case 'true':
-        case '1':
-            return true;
-        default:
-            return false;
-    }
-}
-
 export function validateBoolean(value:any) {
     return value === true ||
            value === '1' ||
