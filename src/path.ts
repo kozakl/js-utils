378768805f75b8ab67c5eac3302752b7aca39f51
Luke Kozak <kozakluke@gmail.com>
2021-02-16 11:28:38 +0100

    Fix join

--- src/path.ts
+++ src/path.ts
@@ -1,9 +1,13 @@
 export function join(...args:string[]) {
-    return args.map((part, index)=> {
-        if (index === 0) {
-            return part.trim().replace(/[\/]*$/g, '')
-        } else {
-            return part.trim().replace(/(^[\/]*|[\/]*$)/g, '')
-        }
-    }).filter((x)=> x.length).join('/')
+    return args
+        .filter((part)=>
+            part && part.length)
+        .map((part, index)=> {
+            if (index === 0) {
+                return part.trim().replace(/[\/]*$/g, '')
+            } else {
+                return part.trim().replace(/(^[\/]*|[\/]*$)/g, '')
+            }
+        })
+        .join('/')
 }
